a7c5df206da4a803a1915f44a5ebf6011044b682
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-14 12:46:38 +0300

    Window Interface

---
+++ WoowzLib/WoowzLib.Window.GLFW/WLO/Window_GLFW.cs
@@ -0,0 +1,20 @@
+﻿using WLO;
+
+namespace WoowzLib.Window.GLFW.WLO;
+
+public class Window_GLFW : WLI.Window{
+    public void Create(int W, int H, string Title){
+        throw new NotImplementedException();
+    }
+    public void Close(){
+        throw new NotImplementedException();
+    }
+    public bool IsClosed{ get; }
+    public Vector2I Size{ get; }
+    public void PollEvents(){
+        throw new NotImplementedException();
+    }
+    public void SwapBuffers(){
+        throw new NotImplementedException();
+    }
+}
---
+++ WoowzLib/WoowzLib.Window/WLI/Window.cs
@@ -0,0 +1,15 @@
+﻿using WLO;
+
+namespace WLI;
+
+public interface Window{
+    void Create(int W, int H, string Title);
+    
+    void Close();
+
+    bool IsClosed{ get; }
+    Vector2I Size{ get; }
+
+    void PollEvents();
+    void SwapBuffers();
+}
