58c171a03ddde559954cc7b8af2327c7b190ecce
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-16 12:19:34 +0300

    OpenGL DebugLogger

--- WoowzLib/WoowzLib.Render.OpenGL/WL/OpenGL.cs
+++ WoowzLib/WoowzLib.Render.OpenGL/WL/OpenGL.cs
@@ -1,5 +1,29 @@
-﻿namespace WL;
+﻿using Silk.NET.OpenGL;
+using WLO;
+
+namespace WL;
 
 public static class OpenGL{
-    
+    public static GL CreateAPI(Func<string, IntPtr> ProcessLoader){
+        GL API = GL.GetApi(ProcessLoader);
+        if(API == null!){ throw new ExceptionWL($"GL.GetApi({ProcessLoader}) вернул null!"); }
+        return API;
+    }
+
+    /// <summary>
+    /// todo
+    /// </summary>
+    /// <param name="API">todo</param>
+    /// <param name="Synchronous">Ловить ошибку сразу, а не позже</param>
+    /// <param name="UserParam">todo</param>
+    public static unsafe void SetupDebugLogger(GL API, DebugProc DebugLogger, bool Synchronous = true, void* UserParam = null){
+        API.Enable(EnableCap.DebugOutput);
+        
+        if(Synchronous){ API.Enable(EnableCap.DebugOutputSynchronous); }
+        
+        API.DebugMessageCallback(DebugLogger, UserParam);
+        
+        // todo, фильтрация
+        //API.DebugMessageControl();
+    }
 }
--- WoowzLib/WoowzLib.Render.OpenGL/WLO/Render/Hardware/OpenGL.cs
+++ WoowzLib/WoowzLib.Render.OpenGL/WLO/Render/Hardware/OpenGL.cs
@@ -1,4 +1,5 @@
-﻿using Silk.NET.OpenGL;
+﻿using System.Runtime.InteropServices;
+using Silk.NET.OpenGL;
 using WLI_Render;
 using WLI.GPU;
 using WLO.GPU;
@@ -10,37 +11,133 @@ using Texture = WLI.GPU.Texture;
 namespace WLO.Render.Hardware;
 
 public class OpenGL : WLI_Render.Hardware{
-    public  GL API => __API;
-    private GL        __API;
-    
-    public RenderView CurrentRenderView{ get; private set; }
-    
-    public bool IsStarted{ get; private set; }
+    #region Значения
 
-    private Func<string, IntPtr> __ProcLoader;
-    public OpenGL(Func<string, IntPtr> ProcLoader){
-        __ProcLoader = ProcLoader;
-    }
-    
-    public void Start(){
-        if(IsStarted){ throw new ExceptionWL("todo"); }
+        public GL API{ get; private set; } = null!;
 
-        __API = GL.GetApi(__ProcLoader);
+        public readonly Func<string, IntPtr> API_ProcessLoader;
         
-        CurrentRenderView = new GLRenderView(this);
         
-        IsStarted = true;
-    }
-    
-    public void Stop(){
-        if(!IsStarted){ throw new ExceptionWL("todo"); }
+        public RenderView CurrentRenderView{ get; private set; } = null!;
 
-        IsStarted = false;
-    }
+        #region Логирование
+
+            public WLI.Logger? CurrentLogger = null!;
+        
+            public DebugProc API_DebugLogger{ get; private set; } = null!;
+            
+            public readonly bool API_HasDebugLogger;
+
+            public uint LogType_Initialization    = (uint)WLI.Logger.Type.Info;
+            public uint LogType_InitDetails       = (uint)WLI.Logger.Type.Info;
+            public uint LogType_DebugLogger_Info  = (uint)WLI.Logger.Type.Info;
+            public uint LogType_DebugLogger_Warn  = (uint)WLI.Logger.Type.Warning;
+            public uint LogType_DebugLogger_Error = (uint)WLI.Logger.Type.Error;
+            public uint LogType_DebugLogger_Fatal = (uint)WLI.Logger.Type.Fatal;
+
+            public bool DebugLogger_ThrowExceptionOnFatalErrors = true;
+            
+        #endregion
+        
+    #endregion
+    // ----------------------------------------------------------------------
+    #region Запуск/Остановка
+    
+        public bool IsStarted{ get; private set; }
+    
+        public OpenGL(Func<string, IntPtr> ProcessLoader, StartParameters? Parameters = null, bool StartImmediately = false){
+            StartParameters Parameters__ = new StartParameters{
+                DebugLogger = Parameters.HasValue && Parameters.Value.DebugLogger.HasValue ? Parameters.Value.DebugLogger.Value : false,
+                UseThisLogger = Parameters.HasValue && Parameters.Value.UseThisLogger != null ? Parameters.Value.UseThisLogger : WL.Logger.CurrentLogger
+                
+            };
+            
+            API_ProcessLoader = ProcessLoader;
+            API_HasDebugLogger = Parameters__.DebugLogger.Value;
+
+            CurrentLogger = Parameters__.UseThisLogger;
+
+            if(StartImmediately){ Start(); }
+        }
+        public struct StartParameters{
+            public bool?       DebugLogger;
+            public WLI.Logger? UseThisLogger;
+        }
+        
+        public unsafe void Start(){
+            try{
+                if(IsStarted){ throw new ExceptionWL("Рендер OpenGL уже и так был запущен!"); }
+
+                Log(LogType_Initialization, "Запуск OpenGL...");
+                
+                Log(LogType_InitDetails, $"Параметры! HasDebugLogger: {API_HasDebugLogger}");
+                
+                API = WL.OpenGL.CreateAPI(API_ProcessLoader);
+                Log(LogType_InitDetails, $"API: {API}, ProcessLoader: {API_ProcessLoader}");
+                
+                if(API_HasDebugLogger){
+                    API_DebugLogger = __DebugLoggerCallback;
+                    WL.OpenGL.SetupDebugLogger(API, API_DebugLogger);
+                    Log(LogType_InitDetails, $"DebugLogger: {API_DebugLogger}");
+                }
+                
+                CurrentRenderView = new GLRenderView(this);
+                Log(LogType_InitDetails, $"CurrentRenderView: {CurrentRenderView}");
+            
+                Log(LogType_Initialization, "OpenGL запущен!");
+                
+                IsStarted = true;
+            }catch(Exception e){
+                throw new ExceptionWL("Произошла ошибка при запуске OpenGL!", e);
+            } 
+        }
+        
+        public void Stop(){
+            try{
+                if(!IsStarted){ throw new ExceptionWL("Рендер OpenGL даже не был запущен!"); }
+                
+                Log(LogType_Initialization, "Остановка OpenGL...");
+                
+                API = null!;
+                
+                Log(LogType_Initialization, "OpenGL остановлен!");
+                
+                IsStarted = false;
+            }catch(Exception e){
+                throw new ExceptionWL("Произошла ошибка при остановке OpenGL!", e);
+            } 
+        }
+    
+    #endregion
+    // ----------------------------------------------------------------------
+    #region Остальное
+
+        public void Log(uint Type, object Message, string Prefix = "GL"){
+            CurrentLogger?.PrefixPush(Prefix);
+            CurrentLogger?.Log(Type, Message);
+            CurrentLogger?.PrefixPop();
+        }
+
+        private void __DebugLoggerCallback(GLEnum Source, GLEnum Type, int ID, GLEnum Severity, int L, nint MessagePtr, nint UserParam){
+            string Message = Marshal.PtrToStringAnsi(MessagePtr, L);
+
+            uint LogType = Severity switch{
+                GLEnum.DebugSeverityHigh when DebugLogger_ThrowExceptionOnFatalErrors => throw new ExceptionWL($"Фатальная ошибка OpenGL: {Message}"),
+                GLEnum.DebugSeverityHigh   => LogType_DebugLogger_Fatal,
+                GLEnum.DebugSeverityMedium => LogType_DebugLogger_Error,
+                GLEnum.DebugSeverityLow    => LogType_DebugLogger_Warn,
+                var _                      => LogType_DebugLogger_Info
+            };
+
+            Log(LogType, Message, "GLDL");
+        }
+    
+    #endregion
+    // ----------------------------------------------------------------------
 
     public void FrameStart(RenderView? Target = null){
         RenderView View = Target ?? CurrentRenderView;
-        __API.Viewport(0, 0, (uint)View.Viewport.X, (uint)View.Viewport.Y);
+        API.Viewport(0, 0, (uint)View.Viewport.X, (uint)View.Viewport.Y);
     }
     
     public void FrameStop(){
